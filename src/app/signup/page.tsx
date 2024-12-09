0a92341dd73826bdbe19c418e3008543e0aaebf4
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-12-10 01:44:36 +0500

    Add files via upload
    first

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import React from "react"
 import Image from "next/image"
-import google from "../../../public/google.png"
-import apple from "../../../public/apple.png"
 
 export default function SignUpPage() {
   return (
